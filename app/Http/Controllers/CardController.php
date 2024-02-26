e381defda41dde7e73f93700212b69002c1febf6
proctorlee250 <105924570+proctorlee250@users.noreply.github.com>
2024-02-26 18:22:05 +0000

    Getting the cards from API
    Getting the cards from API

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CardController extends Controller
 {
@@ -11,6 +12,26 @@ class CardController extends Controller
      */
     public function index()
     {
+        
+    $json = file_get_contents('https://higherorlower-api.netlify.app/json');
+
+    $cards = json_decode($json);
+
+    $shuffled = shuffle($cards);
+
+    echo '<pre>';
+    print_r($cards);
+    echo '</pre>';
+
+    $firstCard = $cards[0];
+
+    echo '<pre>';
+    print_r($firstCard);
+    echo '</pre>';
+    
+
+    exit;
+
         return view('cards')->with('users', $users);
     }
 }
